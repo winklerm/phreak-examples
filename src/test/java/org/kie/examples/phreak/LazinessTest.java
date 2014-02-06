4ffe16f8f96473c90c359d3714664b03f78d149c
Marek Winkler <winkler.marek@gmail.com>
2014-02-06 09:47:48 +0100

    Added initial version of examples

---
+++ src/test/java/org/kie/examples/phreak/LazinessTest.java
@@ -0,0 +1,36 @@
+package org.kie.examples.phreak;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+import org.kie.examples.phreak.util.DataGenerator;
+
+/**
+ * Illustrates laziness of PHREAK vs. eagerness of RETE-OO on a larger set of facts.
+ */
+public class LazinessTest {
+
+    private KieSession ksession;
+
+    @Before
+    public void initSession() {
+        final KieContainer kcontainer = KieServices.Factory.get().getKieClasspathContainer();
+        this.ksession = kcontainer.newKieSession("lazinessKSession");
+    }
+
+    @After
+    public void disposeSession() {
+        if (this.ksession != null) {
+            this.ksession.dispose();
+        }
+    }
+
+    @Test
+    public void testLaziness() {
+        new DataGenerator(this.ksession).insertTestFacts();
+        this.ksession.fireAllRules();
+    }
+}
---
+++ src/test/java/org/kie/examples/phreak/util/DataGenerator.java
@@ -0,0 +1,51 @@
+package org.kie.examples.phreak.util;
+
+import org.kie.api.runtime.KieSession;
+import org.kie.examples.phreak.domain.Account;
+import org.kie.examples.phreak.domain.Person;
+import org.kie.examples.phreak.domain.Transaction;
+
+/**
+ * Utility class generating test facts.
+ */
+public class DataGenerator {
+
+    private final KieSession ksession;
+
+    public DataGenerator(final KieSession ksession) {
+        this.ksession = ksession;
+    }
+
+    public void insertTestFacts() {
+        final Person person1 = this.insertPerson("Marek");
+        final Person person2 = this.insertPerson("Egon");
+        
+        this.insertPersonAccounts(person1, person2);
+        this.insertPersonAccounts(person2, person1);
+    }
+
+    private Person insertPerson(final String name) {
+        final Person person = new Person(name);
+        this.ksession.insert(person);
+        return person;
+    }
+    
+    private void insertPersonAccounts(final Person owner, final Person partner) {
+        for (long i = 0; i < 10; i++) {
+            this.insertAccount(i, owner, partner);
+        }
+    }
+
+    private void insertAccount(final long id, final Person owner, final Person partner) {
+        final Account account = new Account(id, owner);
+        this.ksession.insert(account);
+
+        final Account partnerAccount = new Account(id, partner);
+        this.ksession.insert(partnerAccount);
+        
+        for (int t = 0; t < 10100; t++) {
+            final Transaction transaction = new Transaction(account, partnerAccount, 5);
+            this.ksession.insert(transaction);
+        }
+    }
+}

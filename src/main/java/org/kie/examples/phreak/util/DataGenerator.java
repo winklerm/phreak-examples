9e30eb67df3506972df58730874238ad645be3fa
Lukáš Petrovický <lpetrovi@redhat.com>
2014-03-11 09:23:57 -0400

    DataGenerator item size now instance-specific

--- src/main/java/org/kie/examples/phreak/util/DataGenerator.java
+++ src/main/java/org/kie/examples/phreak/util/DataGenerator.java
@@ -8,31 +8,39 @@ import org.kie.examples.phreak.domain.Transaction;
 /**
  * Utility class generating test facts and inserting them into given KieSession:
  * <ul>
- *   <li>2 facts of type Person,</li>
- *   <li>each having 10 Accounts,</li>
- *   <li>each Account is associated with a number of Transactions (see below).</li>
+ * <li>2 facts of type Person,</li>
+ * <li>each having 10 Accounts,</li>
+ * <li>each Account is associated with a number of Transactions (see below).</li>
  * </ul>
- *
+ * 
  * The number of transactions associated with each account is specified using
  * <em>phreak.examples.transactions</em> system property (default 10100).
  */
 public class DataGenerator {
 
-    private static final Long TRANSACTIONS_PER_ACCOUNT =
-            Long.valueOf(System.getProperty("phreak.examples.transactions", "10100"));
-    
+    private final Long transactionsPerAccount;
+
     private static final int ACCOUNTS_PER_PERSON = 10;
 
     private final KieSession ksession;
 
-    public DataGenerator(final KieSession ksession) {
+    public DataGenerator(final KieSession ksession, final long transactionsPerAccount) {
         this.ksession = ksession;
+        if (transactionsPerAccount < 0) {
+            this.transactionsPerAccount = Long.valueOf(System.getProperty("phreak.examples.transactions", "10100"));
+        } else {
+            this.transactionsPerAccount = transactionsPerAccount;
+        }
+    }
+
+    public DataGenerator(final KieSession ksession) {
+        this(ksession, -1);
     }
 
     public void insertTestFacts() {
         final Person person1 = this.insertPerson("Carl");
         final Person person2 = this.insertPerson("Egon");
-        
+
         this.insertPersonAccounts(person1, person2);
         this.insertPersonAccounts(person2, person1);
     }
@@ -42,9 +50,9 @@ public class DataGenerator {
         this.ksession.insert(person);
         return person;
     }
-    
+
     private void insertPersonAccounts(final Person owner, final Person partner) {
-        for (long i = 0; i < ACCOUNTS_PER_PERSON; i++) {
+        for (long i = 0; i < DataGenerator.ACCOUNTS_PER_PERSON; i++) {
             this.insertAccount(i, owner, partner);
         }
     }
@@ -55,8 +63,8 @@ public class DataGenerator {
 
         final Account partnerAccount = new Account(id, partner);
         this.ksession.insert(partnerAccount);
-        
-        for (long t = 0; t < TRANSACTIONS_PER_ACCOUNT; t++) {
+
+        for (long t = 0; t < this.transactionsPerAccount; t++) {
             final Transaction transaction = new Transaction(account, partnerAccount, 5);
             this.ksession.insert(transaction);
         }

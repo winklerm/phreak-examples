0feb4b73ab56e4e34b5d2593e0917db42f34bc9f
Lukáš Petrovický <lpetrovi@redhat.com>
2014-03-12 19:57:06 -0400

    Tests are run on both engines
    Shows some weir behavior.

--- src/test/java/org/kie/examples/phreak/BenchmarkTest.java
+++ src/test/java/org/kie/examples/phreak/BenchmarkTest.java
@@ -10,35 +10,47 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
+import org.kie.api.KieBase;
 import org.kie.api.runtime.KieSession;
 import org.kie.examples.phreak.util.DataGenerator;
 
 @RunWith(Parameterized.class)
 public class BenchmarkTest {
+    
+    private enum Engine {
+        
+        PHREAK, RETEOO;
+        
+    }
 
     // will verify various configs of log watch
-    @Parameters(name = "{index}: {0}")
+    @Parameters(name = "{index}: {0} with {1}")
     public static Collection<Object[]> data() {
         final List<Object[]> result = new LinkedList<Object[]>();
-        for (final BenchmarkType b : BenchmarkType.values()) {
-            result.add(new Object[] { b });
+        for (Engine e: Engine.values()) {
+            for (final BenchmarkType b : BenchmarkType.values()) {
+                result.add(new Object[] { b, e });
+            }
         }
         return result;
     }
 
+    private Engine engine;
     private KieSession ksession;
     private final DataGenerator data;
     private final BenchmarkType benchmarkType;
 
-    public BenchmarkTest(final BenchmarkType benchmarkType) {
+    public BenchmarkTest(final BenchmarkType benchmarkType, final Engine engine) {
         this.benchmarkType = benchmarkType;
+        this.engine = engine;
         this.data = new DataGenerator(10000);
         this.data.fillWithTestFacts();
     }
 
     @Before
     public void initSession() {
-        this.ksession = this.benchmarkType.getPhreakKieBase().newKieSession();
+        KieBase base = (engine == Engine.PHREAK) ? this.benchmarkType.getPhreakKieBase() : this.benchmarkType.getReteOOKieBase();
+        this.ksession = base.newKieSession();
     }
 
     @After
@@ -49,7 +61,7 @@ public class BenchmarkTest {
     }
 
     @Test
-    public void testGrouping() {
+    public void test() {
         this.benchmarkType.execute(this.data, this.ksession);
     }
 
